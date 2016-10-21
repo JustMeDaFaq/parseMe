7410335afe571c16627d0117203c56244698bb20
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-21 20:49:14 +0200

    Wartezeit nicht mehr benötigt - entfernt!

--- vivosx.ts
+++ vivosx.ts
@@ -1,7 +1,6 @@
 var http = new XMLHttpRequest();
 http.open("GET", url,true);
 http.onloadend = function() {
-
       var splitted=http.responseText.split('"');
       var lnk=splitted[splitted.findIndex(x=>x.endsWith("data-url="))+1]; //der link
       doOnComplete(lnk);         
