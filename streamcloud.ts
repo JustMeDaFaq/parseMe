8017aa948aa9ddc32817ec229ed3b05bf93d0822
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-21 14:11:46 +0200

    Create streamcloud.ts

---
+++ streamcloud.ts
@@ -0,0 +1,27 @@
+var http = new XMLHttpRequest();
+http.open("GET", url,false);
+       http.onloadend = function() {
+          var temp=http.responseText.split('"');
+          var id=temp[temp.lastIndexOf("id")+ 2];
+               var PostData = function ( id, url,doOnComplete){
+                        var http = new XMLHttpRequest();
+                        var data = new FormData();
+                        http.open("POST", url, true);
+                        data.append('op', 'download2');
+                        data.append('id', id);
+                        data.append('imhuman', 'Weiter zum Video');
+                        http.onloadend = function() {
+                          var temp=http.responseText.split('"');
+                            var link=temp.find(x => x.endsWith("video.mp4"));
+                            doOnComplete(link);
+                          
+                        } 
+
+                        http.send(data);
+                    }
+               PostData(id,url,doOnComplete);
+
+           
+        
+       } 
+      http.send();
