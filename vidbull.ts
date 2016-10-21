9743ea18be79d68b74aa46ce5f80d985e5bfa86d
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-21 14:21:50 +0200

    Create vidbull.ts

---
+++ vidbull.ts
@@ -0,0 +1,13 @@
+var http = new XMLHttpRequest();
+
+      http.open("GET", url,true);
+    
+       http.onloadend = function() {
+            var temp=http.responseText.split('"');
+            var lnk=temp.find(x=>(x.endsWith(".mp4")||x.endsWith(".mkv")));
+            doOnComplete(lnk);
+
+            
+
+       } 
+      http.send();
