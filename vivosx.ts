e8177c6a87a8bf4f1cfab38fe7260b962de11f2d
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-25 01:07:16 +0200

    Update vivosx.ts

--- vivosx.ts
+++ vivosx.ts
@@ -1,11 +1,12 @@
-if(!url.startsWith("https"))
-      url=url.replace("http","https");
-console.log(url);
-var http = new XMLHttpRequest();
-http.open("GET", url,true);
-http.onloadend = function() {
-      var splitted=http.responseText.split('"');
-      var lnk=splitted[splitted.findIndex(x=>x.endsWith("data-url="))+1]; //der link
-      doOnComplete(lnk);         
-} 
-http.send();
+    if(!url.startsWith("https"))
+          url=url.replace("http","https");
+    var http = new XMLHttpRequest();
+    http.open("GET", url,true);
+    http.onloadend = function() {
+          var splitted=http.responseText.split("'");
+          var lnk=splitted[splitted.findIndex(x=>x.endsWith("Core.InitializeStream ("))+1]; //der link
+          var lnks = JSON.parse(window.atob(lnk));
+          doOnComplete(lnks[1]);  
+         // console.log(window.atob(lnk));        
+    } 
+    http.send();
