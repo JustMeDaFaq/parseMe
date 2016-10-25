cd9ffa353622028b29cdbb9aefa19bc19deef700
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-25 17:15:13 +0200

    Update flashx.ts

--- flashx.ts
+++ flashx.ts
@@ -1,10 +1,18 @@
-           url=url.replace(".tv/",".me/playvid-");
+url=url.replace(".tv/",".me/playvid-");
             if(!url.endsWith("html"))
               url=url+".html";
             var http = new XMLHttpRequest();
             http.open("GET", url, true);
             http.onloadend = function() {
-                      console.log(http.responseText)
+              console.log(http.responseText);
+              if(http.responseText.indexOf("reloadit")!=-1){
+                 alert("reload");
+                  var tempR=http.responseText.split('"');
+                  var rLink = tempR.find(x=>x.startsWith("./reloadit"))
+                  var fLink="http://www.flashx.tv/"+rLink.replace(".","");
+                  console.log(fLink);
+              }
+                      
             var result=http.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
             var temp= result.split(",")
                     
@@ -26,8 +34,8 @@
                 if(p.indexOf("mobile.mp4")!== -1)
                     var lnk=temp.find(x=>x.endsWith("mobile.mp4"));
                 else
-                    var lnk=temp.find(x=>x.endsWith("high.mp4"));
-                console.log("dsdadasdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd");
-                doOnComplete(lnk)     ;
+                    var lnk=temp.find(x=>x.endsWith(".mp4"));
+
+                doOnComplete(lnk);
              }
         http.send();
