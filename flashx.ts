19e79f055560118a4ff8581b3706b90d917b886f
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-26 23:45:30 +0200

    Update flashx.ts

--- flashx.ts
+++ flashx.ts
@@ -1,5 +1,4 @@
-     var srcc="";
-        url=url.replace(".tv/",".me/playvid-");
+    url=url.replace(".tv/",".me/playvid-");
             if(!url.endsWith("html"))
               url=url+".html";
                   console.log(url);
@@ -18,13 +17,39 @@
                             httpE.open("GET", fLink, false);
                             httpE.send();
                                      var httpD= new XMLHttpRequest();
-                                      httpD.open("GET", url, false);
-                                           httpD.send();
-                                           srcc=httpD.responseText;
+                                      httpD.open("GET", url, true);
+                                      httpD.onloadend=function(){
+                                        
+                                          console.log(httpD.responseText);
+                                                                        var result=httpD.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
+                                          var temp= result.split(",")
+                                                  
+                                          var p ="";
+                                          var a=0;
+                                          var c=0;
+                                          var k;
+                                          temp.forEach(element => {
+                                              if(!isNaN(Number(element)))
+                                                  if(a==0 && Number(element)>20)
+                                                        a=Number(element);
+                                                  else 
+                                                        c=Number(element);
+                                                  });
+                                              p=result.split("',"+a+","+c+",'")[0];
+                                              k=result.split("',"+a+","+c+",'")[1].split("|");           
+                                              while(c--)if(k[c])p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])
+                                              var temp=p.split('"');
+                                              if(p.indexOf("mobile.mp4")!== -1)
+                                                  var lnk=temp.find(x=>x.endsWith("mobile.mp4"));
+                                              else
+                                                  var lnk=temp.find(x=>x.endsWith(".mp4"));
 
-              }else
-                                  srcc=http.responseText;
-                    var result=srcc.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
+                                              doOnComplete(lnk);
+                                      }
+                                      httpD.send();
+
+              }else{
+                                  var result=http.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
                     var temp= result.split(",")
                             
                     var p ="";
@@ -47,8 +72,8 @@
                         else
                             var lnk=temp.find(x=>x.endsWith(".mp4"));
 
-                        alert(lnk);
-                
+                        doOnComplete(lnk);
+                }
                       
           
              }
