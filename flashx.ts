4c20813959a3b01b843d7fe834a84b9815cc7a07
JustMeDaFaq <paypalmax1991@yahoo.de>
2016-10-29 20:02:05 +0200

    Update flashx.ts

--- flashx.ts
+++ flashx.ts
@@ -1,55 +1,26 @@
-    url=url.replace(".tv/",".me/playvid-");
-            if(!url.endsWith("html"))
-              url=url+".html";
-                  console.log(url);
-            var http = new XMLHttpRequest();
-            http.open("GET", url, true);
+var http = new XMLHttpRequest();        
+http.open("GET", url,true);
             http.onloadend = function() {
-              if(http.responseText.indexOf("reloadit")!=-1){
-                 console.log("reload");
+                var temp=http.responseText.split('"');
+                var id=temp[temp.findIndex(x=>x=="id")+2];
+                var fname=temp[temp.findIndex(x=>x=="fname")+2];
+                var hash=temp[temp.findIndex(x=>x=="hash")+2];
+                
+                setTimeout(function() {
+                    http = new XMLHttpRequest();
+                    var data = new FormData();
+                    http.open("POST", url, true);
+                    data.append('op', "download1");
+                    data.append('usr_login', '');
+                    data.append('id', id);
+                    data.append('fname', fname);
+                    data.append('referer', '');
+                    data.append('hash', hash);
 
-                     var tempR=http.responseText.split('"');
-                  var rLink = tempR.find(x=>x.startsWith("./reloadit"))
-                  var fLink="http://www.flashx.tv"+rLink.replace(".","");
-                           
-                  var httpE = new XMLHttpRequest();
-                           console.log("http://www.flashx.tv"+rLink.replace(".",""));
-                            httpE.open("GET", fLink, false);
-                            httpE.send();
-                                     var httpD= new XMLHttpRequest();
-                                      httpD.open("GET", url, true);
-                                      httpD.onloadend=function(){
-                                        
-                                          console.log(httpD.responseText);
-                                                                        var result=httpD.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
-                                          var temp= result.split(",")
-                                                  
-                                          var p ="";
-                                          var a=0;
-                                          var c=0;
-                                          var k;
-                                          temp.forEach(element => {
-                                              if(!isNaN(Number(element)))
-                                                  if(a==0 && Number(element)>20)
-                                                        a=Number(element);
-                                                  else 
-                                                        c=Number(element);
-                                                  });
-                                              p=result.split("',"+a+","+c+",'")[0];
-                                              k=result.split("',"+a+","+c+",'")[1].split("|");           
-                                              while(c--)if(k[c])p=p.replace(new RegExp('\\b'+c.toString(a)+'\\b','g'),k[c])
-                                              var temp=p.split('"');
-                                              if(p.indexOf("mobile.mp4")!== -1)
-                                                  var lnk=temp.find(x=>x.endsWith("mobile.mp4"));
-                                              else
-                                                  var lnk=temp.find(x=>x.endsWith(".mp4"));
-
-                                              doOnComplete(lnk);
-                                      }
-                                      httpD.send();
-
-              }else{
-                                  var result=http.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
+                    http.onloadend = function() {
+                        
+                     //   var tt=http.responseText.split("eval(")[1].split("</script>")[0];
+                    var result=http.responseText.split("p,a,c,k,e,d){while")[1].split("'.split")[0];
                     var temp= result.split(",")
                             
                     var p ="";
@@ -72,9 +43,12 @@
                         else
                             var lnk=temp.find(x=>x.endsWith(".mp4"));
 
-                        doOnComplete(lnk);
-                }
-                      
-          
-             }
-        http.send();
+                        console.log(lnk);
+                        console.log(k);
+                    } 
+
+                    http.send(data);
+                }, 8000);
+
+            } 
+            http.send();   
